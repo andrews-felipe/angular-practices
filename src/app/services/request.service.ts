e85b5e4a7c325a306c71b4bdb18dffdc838027d7
Felipe Andrew <ferulip@gmail.com>
2019-01-31 17:40:21 -0300

    Corrigindo métodos do service request

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { API_URL } from 'src/environments/endpoints';
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +12,23 @@ export class RequestService {
 
 
   get(endpoint : string) : Observable<any>{
-    return this.http.get(`${endpoint}${endpoint}`)
+    return this.http.get(`${API_URL}${endpoint}`)
   }
 
   getById(endpoint : string, id : string) : Observable<any>{
-    return this.http.get(`${endpoint}${endpoint}/${id}`)
+    return this.http.get(`${API_URL}${endpoint}/${id}`)
   }
 
   post(endpoint : string, object : any){
-    return this.http.post(`${endpoint}${endpoint}`, object)
+    return this.http.post(`${API_URL}${endpoint}`, object)
   }
 
-  put(endpoint : string, object : any){
-    return this.http.put(`${endpoint}${endpoint}`, object)
+  put(endpoint : string, id : string , object : any){
+    return this.http.put(`${endpoint}${endpoint}/${id}`, object)
   }
 
-  delete(endpoint : string, object : any){
-    return this.http.delete(`${endpoint}${endpoint}`, object)
+  delete(endpoint : string, id : string){
+    return this.http.delete(`${API_URL}${endpoint}/${id}`)
   }
 
 }
